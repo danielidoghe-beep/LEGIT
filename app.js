4a30f09cfde5e44a655826da35854f4f1e532935
danielidoghe-beep <danielidoghe@gmail.com>
2026-06-28 18:14:01 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOvI9756xstsLc7ZMou75pSDd5ZZU0cfg",
+  authDomain: "legit-ee0ae.firebaseapp.com",
+  projectId: "legit-ee0ae",
+  storageBucket: "legit-ee0ae.firebasestorage.app",
+  messagingSenderId: "389932442399",
+  appId: "1:389932442399:web:dc97b02a8e197deca0862a"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
