3370683c42b17fcdc1e2e958cc9edca024f17e22
danielidoghe-beep <danielidoghe@gmail.com>
2026-06-29 06:40:30 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -8,7 +8,11 @@ import {
 import {
   getFirestore,
   doc,
-  getDoc
+  getDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -33,14 +37,40 @@ onAuthStateChanged(auth, async (user) => {
   document.getElementById("userEmail").textContent =
     "Welcome, " + user.email;
 
-  const docRef = doc(db, "users", user.uid);
-  const docSnap = await getDoc(docRef);
+  const userRef = doc(db, "users", user.uid);
+  const userSnap = await getDoc(userRef);
 
-  if (docSnap.exists()) {
-    const data = docSnap.data();
+  if (userSnap.exists()) {
+    const data = userSnap.data();
 
     document.getElementById("balance").textContent =
       "$" + Number(data.balance).toFixed(2);
+  }
+
+  const transactionList = document.getElementById("transactionList");
+  transactionList.innerHTML = "";
+
+  const q = query(
+    collection(db, "users", user.uid, "transactions"),
+    orderBy("date", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) {
+    transactionList.innerHTML = "<p>No transactions yet.</p>";
+  } else {
+    snapshot.forEach((transaction) => {
+      const data = transaction.data();
+
+      transactionList.innerHTML += `
+        <div class="card">
+          <strong>${data.type}</strong><br>
+          Amount: $${Number(data.amount).toFixed(2)}<br>
+          ${data.date}
+        </div>
+      `;
+    });
   }
 });
 
